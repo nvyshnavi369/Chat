8be8b8f42f806863529356c681a28ea10840df05
nvyshnavi369 <nvyshnavi369@gmail.com>
2020-03-20 13:51:38 +0530

    line spacing

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.chat;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,18 +27,19 @@ public class MainActivity extends AppCompatActivity {
     Button submit1;
     TextView signup;
     SharedPreferences pref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        submit1= findViewById(R.id.submit);
-        this.signup= findViewById(R.id.signup);
+        submit1 = findViewById(R.id.submit);
+        this.signup = findViewById(R.id.signup);
         super.onCreate(savedInstanceState);
-        pref = getSharedPreferences("user_details",MODE_PRIVATE);
-        if(pref.contains("mobile") && pref.contains("password")){
-            store.mobileno=pref.getString("mobile","");
-            store.password1=pref.getString("password","");
+        pref = getSharedPreferences("user_details", MODE_PRIVATE);
+        if (pref.contains("mobile") && pref.contains("password")) {
+            store.mobileno = pref.getString("mobile", "");
+            store.password1 = pref.getString("password", "");
 
-            Intent intent2=new Intent(MainActivity.this,home.class);
-            intent2.putExtra("mobile",pref.getString("mobile",""));
+            Intent intent2 = new Intent(MainActivity.this, home.class);
+            intent2.putExtra("mobile", pref.getString("mobile", ""));
             startActivity(intent2);
         }
 
@@ -45,14 +47,14 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,otp.class);
+                Intent intent = new Intent(MainActivity.this, otp.class);
                 startActivity(intent);
             }
         });
         findViewById(R.id.forgot).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Forgot_password.class);
+                Intent intent = new Intent(MainActivity.this, Forgot_password.class);
                 startActivity(intent);
             }
         });
@@ -61,14 +63,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
-                final EditText mobile1=findViewById(R.id.mobile);
-                final EditText password=findViewById(R.id.password);
-                if(mobile1.getText().toString().trim().isEmpty() || mobile1.getText().toString().trim().length() !=10){
+                final EditText mobile1 = findViewById(R.id.mobile);
+                final EditText password = findViewById(R.id.password);
+                if (mobile1.getText().toString().trim().isEmpty() || mobile1.getText().toString().trim().length() != 10) {
                     mobile1.setError("Enter valid mobile number");
                     mobile1.requestFocus();
                     return;
                 }
-                if(password.getText().toString().trim().isEmpty()){
+                if (password.getText().toString().trim().isEmpty()) {
                     password.setError("Enter correct password");
                     password.requestFocus();
                     return;
@@ -81,21 +83,20 @@ public class MainActivity extends AppCompatActivity {
                             DocumentSnapshot document = task.getResult();
                             if (document.exists()) {
                                 Log.d("TAG", "DocumentSnapshot data: " + document.getData());
-                               Map<String, Object> user = document.getData();
-                                if(user.get("password").equals(password.getText().toString().trim())){
+                                Map<String, Object> user = document.getData();
+                                if (user.get("password").equals(password.getText().toString().trim())) {
 
-                                    store.mobileno=mobile1.getText().toString().trim();
-                                    store.password1=password.getText().toString().trim();
+                                    store.mobileno = mobile1.getText().toString().trim();
+                                    store.password1 = password.getText().toString().trim();
                                     SharedPreferences.Editor editor = pref.edit();
-                                    editor.putString("mobile",mobile1.getText().toString().trim());
-                                    editor.putString("password",password.getText().toString().trim());
+                                    editor.putString("mobile", mobile1.getText().toString().trim());
+                                    editor.putString("password", password.getText().toString().trim());
                                     editor.commit();
                                     //Toast.makeText(MainActivity.this,store.mobileno,Toast.LENGTH_SHORT).show();
-                                    Intent intent=new Intent(MainActivity.this,home.class);
-                                    intent.putExtra("mobile",mobile1.getText().toString().trim());
+                                    Intent intent = new Intent(MainActivity.this, home.class);
+                                    intent.putExtra("mobile", mobile1.getText().toString().trim());
                                     startActivity(intent);
-                                }
-                                else{
+                                } else {
                                     password.setError("the password is incorrect try forgot password");
                                     password.requestFocus();
                                     return;
