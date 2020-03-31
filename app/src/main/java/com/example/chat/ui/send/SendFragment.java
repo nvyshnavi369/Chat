e8d2324249649d6bf9c3dbd7a08b46bd412ef414
nvyshnavi369 <nvyshnavi369@gmail.com>
2020-03-31 22:01:45 +0530

    calenderView

--- app/src/main/java/com/example/chat/ui/send/SendFragment.java
+++ app/src/main/java/com/example/chat/ui/send/SendFragment.java
@@ -9,7 +9,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -26,8 +28,12 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.lang.Long.decode;
+import static java.lang.Long.parseLong;
 
 public class SendFragment extends Fragment {
 
@@ -43,8 +49,34 @@ public class SendFragment extends Fragment {
         final EditText address = root.findViewById(R.id.address);
         final EditText preferences = root.findViewById(R.id.preferences);
         final EditText aadhar = root.findViewById(R.id.aadhar);
+        final CalendarView calendarView = root.findViewById(R.id.calendarView);
+        final Calendar calendar=Calendar.getInstance();
+        calendarView.setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
 
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
 
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+                                // set this date in TextView for Display
+                                dob.setText(Date);
+                            }
+                        });
 
 
 
@@ -71,10 +103,23 @@ public class SendFragment extends Fragment {
                                 //  Toast.makeText(home.this,user.get("password").toString().trim(),Toast.LENGTH_SHORT).show();
                                 name.setText(user.get("name").toString().trim());
                                 address.setText( user.get("address").toString().trim());
-                                dob.setText(user.get("dob").toString().trim());
                                 preferences.setText(user.get("preferences").toString().trim());
                                 aadhar.setText(user.get("aadhar").toString().trim());
+                                String s=user.get("dob").toString().trim();
+                                if(s!="") {
+                                    int i = s.indexOf('-');
+                                    int cdate = Integer.valueOf(s.substring(0, i));
+                                    int j = s.indexOf('-', i + 1);
+                                    int cmonth = Integer.valueOf(s.substring(i + 1, j));
+                                    int cyear = Integer.valueOf(s.substring(j + 1));
 
+
+                                    calendar.set(Calendar.MONTH, cmonth-1);
+                                    calendar.set(Calendar.YEAR, cyear);
+                                    calendar.set(Calendar.DATE, cdate);
+                                    calendarView.setDate(calendar.getTimeInMillis(), false, false);
+                                    dob.setText(cdate+"-"+cmonth+"-"+cyear);
+                                }
 
                                 //  Toast.makeText(home.this, string[0], Toast.LENGTH_SHORT).show();
 
@@ -133,46 +178,7 @@ public class SendFragment extends Fragment {
                 }
             }
             });
-        dob.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                // Insert char where needed.
-
-
-                char space = '/';
-                if (s.length() > 0 && (s.length()  == 3 || (s.length()  == 6 ) )){
-                    final char c = s.charAt(s.length() - 1);
-                    if (space == c) {
-                        s.delete(s.length() - 1, s.length());
-                    }
-                }
-                // Insert char where needed.
-                if (s.length() > 0 && (s.length()  == 3 || (s.length()  == 6 ))) {
-                    char c = s.charAt(s.length() - 1);
-                    // Only if its a digit where there should be a space we insert a space
-                    if (Character.isDigit(c) && TextUtils.split(s.toString(), String.valueOf(space)).length <= 3) {
-                        s.insert(s.length() - 1, String.valueOf(space));
-                    }
-                }
-                if (s.length() > 0 && (s.length() >=11)) {
-                    final char c = s.charAt(s.length() - 1);
-
-                    s.delete(s.length() - 1, s.length());
-
-                }
-            }
-        });
         root.findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
